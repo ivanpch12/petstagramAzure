2c3422d64bedf987cdbf9068f8b9d2a0b63fd5f3
Ivan Chardakov <ivanchardakov@Ivans-MacBook-Pro.local>
2026-03-15 19:15:01 +0200

    feat: added cloudinary

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,4 @@
+from cloudinary.models import CloudinaryField
 from django.contrib.auth import get_user_model
 from django.core.validators import MinLengthValidator
 from django.db import models
@@ -7,12 +8,7 @@ UserModel = get_user_model()
 
 
 class Photo(models.Model):
-    photo = models.ImageField(
-        upload_to='media',
-        validators=[
-            FileSizeValidator(5),
-        ]
-    )
+    photo = CloudinaryField('image')
 
     description = models.CharField(
         max_length=100,
